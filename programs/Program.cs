b32fb41c87fdc3b1f4f526d52c820a448fe3b497
rns <niyazanov@yandex.ru>
2022-10-01 14:15:08 +0300

    функционал формирование массива из строк, по количеству символов

---
+++ programs/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Массив строк");
+string[] arList = new string[4]{"hello", "2", "word",":-)"};
+
+string[] ShortsValues(string[] arList, int columnLength = 3)
+{
+    int count = 0;
+    for(int i = 0; i < arList.Length; i++){
+        if(arList[i].Length <= columnLength){
+            count++;
+        }
+    }
+    string[] arResult = new string[count];
+    int z = 0;
+    for(int i = 0; i < arList.Length; i++){
+        if(arList[i].Length <= columnLength){
+            arResult[z] = arList[i];
+            z++;
+        }
+    }
+    return arResult;
+}
+
+void arPrint(string[] arList)
+{
+    Console.Write("[");
+    for(int i = 0; i < arList.Length; i++){
+        Console.Write(arList[i]+",");
+    }
+    Console.Write("]");
+}
+
+string[] arShortValues = ShortsValues(arList, 3);
+arPrint(arShortValues);
